a48e47a4c703eed71457a5f5f5ab7260a950e83c
Aravind_Sai <153757430+Aravindsai15@users.noreply.github.com>
2024-12-23 00:46:26 +0100

    Update YOLO_object_det.py

--- YOLO_object_det.py
+++ YOLO_object_det.py
@@ -1,3 +1,11 @@
+
+#Download the required dataset and weights
+
+# wget https://pjreddie.com/media/files/yolov3.weights
+# wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true -O yolov3.cfg
+# wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O coco.names
+
+
 import numpy as np
 import cv2
 import time
